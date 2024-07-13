b07b92be2122a445a227adc60e81e202fe1670cb
hashini321 <kaushalyah000@gmail.com>
2024-07-13 10:22:27 +0530

    connecting frontend & backend using redux

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
-import logo from './logo.svg';
 import './App.css';
-import  { Navbar }  from './component/Navbar/Navbar';
 import { darkTheme } from './Theme/DarkTheme';
 import { CssBaseline, ThemeProvider } from '@mui/material';
-import  { Home }  from './component/Home/Home';
-import RestaurantDetails from './component/Restaurant/RestaurantDetails';
-import Cart from './component/Cart/Cart';
-import Profile from './component/Profile/Profile';
 import CustomerRouters from './Routers/CustomerRouters';
 
 
